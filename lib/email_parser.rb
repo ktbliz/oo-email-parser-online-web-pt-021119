076e2b40b76964748d269e00c3a4385a086d1c02
Katie Blizzard <kblizzard11@gmail.com>
2019-03-21 23:29:18 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,17 +13,11 @@ def initialize(emails)
 end 
 
 def parse 
-  if self.emails.split(",").length > 1 
-    emails_array = self.emails.split(", ")
-    emails_array2 = []
-    emails_array.each do |email|
-      emails_array2 << email.split
-    end
-    return emails_array2.flatten.uniq 
-  else 
-    emails_array = self.emails.split(" ")
-    return emails_array.uniq
-  end
+  emails.split.map do | email |
+      email.split(",")
+  end.flatten.uniq 
 end 
+
+
   
 end 
